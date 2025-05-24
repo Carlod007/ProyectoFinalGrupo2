ad9adfdb0c21a60bf5783758fb1eb72e72fe7b66
Carlos Daniel Pasache Sánchez <carlospasachesanchez@gmail.com>
2025-05-23 21:27:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyectofinalgrupo2/BuscadorLabs.kt
@@ -0,0 +1,25 @@
+package com.example.proyectofinalgrupo2
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BuscadorLabs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_buscador_labs)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnBack = findViewById<ImageButton>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
