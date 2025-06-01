231e153d741d41e51d456a17bb5d158a6a9910b7
Carlos Daniel Pasache Sánchez <carlospasachesanchez@gmail.com>
2025-05-31 23:12:45 -0500

    dsad

--- app/src/main/java/com/example/proyectofinalgrupo2/BuscadorLabs.kt
+++ app/src/main/java/com/example/proyectofinalgrupo2/BuscadorLabs.kt
@@ -6,12 +6,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyectofinalgrupo2.databinding.ActivityBuscadorLabsBinding
 
 class BuscadorLabs : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBuscadorLabsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_buscador_labs)
+        binding = ActivityBuscadorLabsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
