7a37356895ca94ccee8972ac66b2fd4bd5593d42
Carlos Daniel Pasache Sánchez <carlospasachesanchez@gmail.com>
2025-05-31 22:13:58 -0500

    Crear carpeta model y data class

---
+++ app/src/main/java/com/example/proyectofinalgrupo2/model/Laboratorios.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinalgrupo2.model
+
+data class Laboratorios(
+    var lab_id: Int,
+    var lab_nombre: String,
+    var lab_codigo: String,
+    var lab_piso: Int,
+    var lab_pabellon: String,
+    var lab_imgsalon: String,
+    var lab_imgrecorrido: String
+)
