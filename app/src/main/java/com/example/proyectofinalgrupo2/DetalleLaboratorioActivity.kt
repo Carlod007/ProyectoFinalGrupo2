cf81c94979da8b29e044ff4d4438e0c9ff1f26dc
Carlos Daniel Pasache Sánchez <carlospasachesanchez@gmail.com>
2025-06-02 17:48:01 -0500

    Agregar ScrollView

--- app/src/main/java/com/example/proyectofinalgrupo2/DetalleLaboratorioActivity.kt
+++ app/src/main/java/com/example/proyectofinalgrupo2/DetalleLaboratorioActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyectofinalgrupo2
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,25 +15,49 @@ class DetalleLaboratorioActivity : AppCompatActivity() {
     private lateinit var tvNombre: TextView
     private lateinit var tvPiso: TextView
     private lateinit var tvPabellon: TextView
+    private lateinit var imgSalon: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detalle_laboratorio)
 
+        //Vincular vistas
         tvNombre = findViewById(R.id.tvNombre)
         tvPiso = findViewById(R.id.tvPiso)
         tvPabellon = findViewById(R.id.tvPabellon)
+        imgSalon = findViewById(R.id.imgSalon)
 
+        //Obtener datos del Intent
         val codigo = intent.getStringExtra("codigo")
         val nombre = intent.getStringExtra("nombre")
         val piso = intent.getIntExtra("piso", 0)
         val pabellon = intent.getStringExtra("pabellon")
+        val imgSalonNombre = intent.getStringExtra("imgSalon")
+        val imageViewSalon = findViewById<ImageView>(R.id.imgSalon)
 
 
+        //Asignar datos a vistas
         tvNombre.text = nombre
         tvPiso.text = "Piso $piso"
         tvPabellon.text = "Pabellón $pabellon"
+
+        if (!imgSalonNombre.isNullOrEmpty()) {
+            // Elimina la extensión .jpg si existe
+            val nombreSinExtension = imgSalonNombre.substringBeforeLast(".")
+            val resourceId = resources.getIdentifier(nombreSinExtension, "drawable", packageName)
+            Log.d("DetalleLaboratorio", "Buscando imagen: $nombreSinExtension con ID: $resourceId")
+
+            if (resourceId != 0) {
+                imageViewSalon.setImageResource(resourceId)
+            } else {
+                imageViewSalon.setImageResource(R.drawable.logo_upn)
+            }
+        } else {
+            imageViewSalon.setImageResource(R.drawable.logo_upn)
+        }
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
